ff95262bef93a7c326cdc1e2e6e144743d54bd06
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-09 23:37:39 +0500

    update

--- src/components/TimerButton/TimerButton.tsx
+++ src/components/TimerButton/TimerButton.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
+import './TimerButton.css';
 
 const TimerButton =(props:any) => (    
     
-  <div className="button-container" />
+  <div className="button-container">
+    <button className='button-value'
+             onClick={props.buttonAction}>
+             {props.buttonValue}
+    </button>
+  </div>
 );
 
 TimerButton.propTypes = {
